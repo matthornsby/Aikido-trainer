3b613d06b96806efe2dd18238a0e827433b29819
Matt Hornsby <matt@matthornsby.com>
2023-06-10 20:21:31 -0400

    spitting out some of the list

--- js/app.js
+++ js/app.js
@@ -1,3 +1,65 @@
-fetch('data/data.json')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+
+async function fetchTechniques() {
+  const response = await fetch('data/data.json');
+
+  response.ok;     // => false
+  response.status; // => 404
+
+  const techniques = await response.json();
+  return techniques;
+}
+
+
+fetchTechniques().then(techniques => {
+  //console.log(techniques);
+  writeList(techniques);
+}
+);
+
+
+function writeList(techniques) {
+
+  
+  // Test to see if the browser supports the HTML template element by checking
+  // for the presence of the template element's content attribute.
+  if ('content' in document.createElement('template')) {
+    // Instantiate the table with the existing HTML tbody
+    // and the row with the template
+    const list = document.querySelector('#list');
+    const tSet = document.querySelector('#technique-set');
+    const tItem = document.querySelector('#technique-item');
+
+
+
+    for (const techniqueSet in techniques) {
+      //console.log(techniques[techniqueSet].name);
+      
+      // clone the new set template
+       const set = tSet.content.cloneNode(true);
+
+       //add the set name
+       set.querySelector('li').prepend(techniques[techniqueSet].name);
+
+       for (const techniqueItem in techniques[techniqueSet]) {
+        //clone the new item template
+        const item = tItem.content.cloneNode(true);
+
+        console.log(techniques[techniqueSet].list)
+
+       }
+
+       list.appendChild(set);
+      
+
+    }
+
+
+
+
+  } else {
+    console.log('no templates');
+    // Find another way to add the rows to the table because
+    // the HTML template element is not supported.
+  }
+}
+
